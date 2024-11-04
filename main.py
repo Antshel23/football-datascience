124e786450ad615789f9fa8ae8ecf6c24a005ce3
Antshel23 <antsheldon23@icloud.com>
2024-11-04 18:44:00 +0000

    added 24/25

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import numpy as np
 from scipy.stats import zscore
 
 
-folder2223_path = '/Users/as/Library/CloudStorage/GoogleDrive-exyhd1@gmail.com/My Drive/Dorking Wanderers FC Scouting/Recruitment/Pipeline/2223'
-folder2324_path = '/Users/as/Library/CloudStorage/GoogleDrive-exyhd1@gmail.com/My Drive/Dorking Wanderers FC Scouting/Recruitment/Pipeline/2324'
+folder2223_path = '/Users/as/Personal projects/FootballProjects/2223/1'
+folder2324_path = '/Users/as/Personal projects/FootballProjects/2324/1'
+folder2425_path = '/Users/as/Personal projects/FootballProjects/2425/1'
 
 def process_data_sets():
     df_2223 = pd.DataFrame()
@@ -26,8 +27,18 @@ def process_data_sets():
                 df_2324 = pd.concat([df_2324, df2], ignore_index=True)
     df_2324.insert(2, 'Season', '23/24')
 
+
+    df_2425 = pd.DataFrame()
+    for root, dirs, files in os.walk(folder2425_path):
+         for file in files:
+            if file.endswith('.xlsx'):
+                file_path = os.path.join(root, file)
+                df3 = pd.read_excel(file_path, engine='openpyxl')
+                df_2425 = pd.concat([df_2425, df3], ignore_index=True)
+    df_2425.insert(2, 'Season', '24/25')
+
     global df
-    df = pd.concat([df_2223, df_2324], ignore_index=True)
+    df = pd.concat([df_2223, df_2324, df_2425], ignore_index=True)
     df = df.drop_duplicates(subset=['Player', 'Minutes played'])
 process_data_sets()
 
@@ -50,7 +61,7 @@ position_clean()
 
 def extra_stats():
     df['xG/shot'] = np.where((df['xG per 90'] == 0) | (df['Shots per 90'] == 0), 0, df['xG per 90'] / df['Shots per 90'])
-    df['xG performance'] = np.where((df['Goals per 90'] == 0) | (df['xG per 90'] == 0), 0, df['Goals per 90'] / df['xG per 90'])
+    df['xG performance'] = np.where((df['Goals per 90'] == 0) | (df['xG per 90'] == 0), 0, df['Goals per 90'] - df['xG per 90'])
     df['Successful dribbles per 90'] = df['Successful dribbles, %'] * df['Dribbles per 90']
     df['Successful crosses per 90'] = df['Accurate crosses, %'] * df['Crosses per 90']
     df['xA/box entry'] = np.where((df['xA per 90'] == 0) | (df['Passes to penalty area per 90'] == 0), 0, df['xA per 90'] / df['Passes to penalty area per 90'])
@@ -60,21 +71,21 @@ def extra_stats():
     df['Through passes per 90'] *= df['Accurate through passes, %'] 
 extra_stats()
 
-def possession_adjust_def():
-    possession_adjust_def = df['Interceptions per 90'] / df['PAdj Interceptions']
-    stats_for_adjust_def = ['Defensive duels per 90', 'Aerial duels per 90', 'Shots blocked per 90', 'Successful defensive actions per 90']
+#def possession_adjust_def():
+   # possession_adjust_def = df['PAdj Interceptions'] / df['Interceptions per 90']
+    #stats_for_adjust_def = ['Defensive duels per 90', 'Aerial duels per 90', 'Shots blocked per 90', 'Successful defensive actions per 90']
     
-    for stat in stats_for_adjust_def:
-        df[stat] = df[stat] / possession_adjust_def
+    #for stat in stats_for_adjust_def:
+        #df[stat] = df[stat] * possession_adjust_def
 
-possession_adjust_def()
+#possession_adjust_def()
 
-def possession_adjust_att():
-    stats_for_adjust_att = ['Successful dribbles per 90', 'Progressive runs per 90', 'Accelerations per 90']
-    for stat in stats_for_adjust_att:
-        df[stat] = df[stat] / df['Received passes per 90']
+#def possession_adjust_att():
+    #stats_for_adjust_att = ['Successful dribbles per 90', 'Progressive runs per 90', 'Accelerations per 90']
+    #for stat in stats_for_adjust_att:
+       # df[stat] = df[stat] / df['Received passes per 90']
 
-possession_adjust_att()
+#possession_adjust_att()
 
 def zscore_manipulate():
     global columns1
@@ -124,19 +135,24 @@ sigmoid_manipulate()
 def assign_league():
     step1_2223 = ['Aldershot Town', 'Altrincham', 'Barnet', 'Boreham Wood', 'Bromley', 'Chesterfield', 'Dagenham & Redbridge', 'Dorking Wanderers', 'Eastleigh', 'Halifax Town', 'Gateshead', 'Maidenhead United', 'Maidstone United', 'Notts County', 'Oldham Athletic', 'Scunthorpe United', 'Solihull Moors', 'Southend United', 'Torquay United', 'Wealdstone', 'Woking', 'Wrexham', 'Yeovil Town', 'York City']
     step1_2324 = ['Fylde', 'Aldershot Town', 'Altrincham', 'Barnet', 'Boreham Wood', 'Bromley', 'Chesterfield', 'Dagenham & Redbridge', 'Dorking Wanderers', 'Eastleigh', 'Ebbsfleet United', 'Gateshead', 'Halifax Town', 'Hartlepool United', 'Kidderminster Harriers', 'Maidenhead United', 'Oldham Athletic', 'Oxford City', 'Rochdale', 'Solihull Moors', 'Southend United', 'Wealdstone', 'Woking', 'York City']
+    step1_2425 = ['Fylde', 'Aldershot Town', 'Altrincham', 'Barnet', 'Boston United', 'Braintree Town', 'Dagenham & Redbridge', 'Eastleigh', 'Ebbsfleet United', 'Forest Green Rovers','Gateshead', 'Halifax Town', 'Hartlepool United', 'Maidenhead United', 'Oldham Athletic', 'Rochdale', 'Solihull Moors', 'Southend United', 'Sutton United', 'Tamworth', 'Wealdstone', 'Woking', 'York City', 'Yeovil Town']
     step2_2223 = ["Fylde", "AFC Telford United", "Alfreton Town", "Banbury United", "Blyth Spartans", "Boston United", "Brackley Town", "Bradford (Park Avenue)", "Buxton", "Chester", "Chorley", "Curzon Ashton", "Darlington 1883", "Farsley Celtic FC", "Gloucester City", "Hereford FC", "Kettering Town", "Kidderminster Harriers", "King's Lynn Town", "Leamington", "Peterborough Sports", "Scarborough Athletic", "Southport", "Spennymoor Town", "Bath City", "Braintree Town", "Chelmsford City", "Cheshunt", "Chippenham Town", "Concord Rangers", "Dartford", "Dover Athletic", "Dulwich Hamlet", "Eastbourne Borough", "Ebbsfleet United", "Farnborough", "Hampton & Richmond", "Havant & Waterlooville", "Hemel Hempstead Town", "Hungerford Town", "Oxford City", "Slough Town", "St. Albans City", "Taunton Town", "Tonbridge Angels", "Welling United", "Weymouth", "Worthing"]
     step2_2324 = ["Alfreton Town", "Banbury United", "Bishop's Stortford", "Blyth Spartans", "Boston United", "Brackley Town", "Buxton", "Chester", "Chorley", "Curzon Ashton", "Darlington 1883", "Farsley Celtic FC", "Gloucester City", "Hereford FC", "King's Lynn Town", "Peterborough Sports", "Rushall Olympic", "Scarborough Athletic", "Scunthorpe United", "South Shields", "Southport", "Spennymoor Town", "Tamworth", "Warrington Town", "Aveley", "Bath City", "Braintree Town", "Chelmsford City", "Chippenham Town", "Dartford", "Dover Athletic", "Eastbourne Borough", "Farnborough", "Hampton & Richmond", "Havant & Waterlooville", "Hemel Hempstead Town", "Maidstone United", "Slough Town", "St. Albans City", "Taunton Town", "Tonbridge Angels", "Torquay United", "Truro City", "Welling United", "Weston-super-Mare", "Weymouth", "Worthing", "Yeovil Town"]
+    step2_2425 = ["Alfreton Town", "Brackley Town", "Buxton", "Chester", "Chorley", "Curzon Ashton", "Darlington 1883", "Farsley Celtic FC", "Hereford FC", "King's Lynn Town", 'Marine', 'Needham Market', 'Oxford City', "Peterborough Sports", 'Radcliffe FC', "Rushall Olympic", "Scarborough Athletic", "Scunthorpe United", "South Shields", "Southport", "Spennymoor Town", "Warrington Town", "Aveley", "Bath City", "Boreham Wood", "Chelmsford City", 'Chesham United', "Chippenham Town", 'Dorking Wanderers', "Eastbourne Borough", 'Enfield Town', "Farnborough", "Hampton & Richmond", "Hemel Hempstead Town", 'Hornchurch', "Maidstone United", 'Salisbury', "Slough Town", "St. Albans City", "Tonbridge Angels", "Torquay United", "Truro City", "Welling United", "Weston-super-Mare", "Weymouth", "Worthing"]
     efl_2223 = ["AFC Wimbledon", "Barrow", "Bradford City", "Carlisle United", "Colchester United", "Crawley Town", "Crewe Alexandra", "Doncaster Rovers", "Gillingham", "Grimsby Town", "Harrogate Town", "Hartlepool United", "Leyton Orient", "Mansfield Town", "Newport County", "Northampton Town", "Rochdale", "Salford City", "Stevenage", "Stockport County", "Sutton United", "Swindon Town", "Tranmere Rovers", "Walsall"]
     efl_2324 = ["Accrington Stanley", "AFC Wimbledon", "Barrow", "Bradford City", "Colchester United", "Crawley Town", "Crewe Alexandra", "Doncaster Rovers", "Forest Green Rovers", "Gillingham", "Grimsby Town", "Harrogate Town", "Mansfield Town", "Milton Keynes Dons", "Morecambe", "Newport County", "Notts County", "Salford City", "Stockport County", "Sutton United", "Swindon Town", "Tranmere Rovers", "Walsall", "Wrexham"]
-
+    efl_2425 = ["Accrington Stanley", "AFC Wimbledon", "Barrow", "Bradford City", 'Bromley', 'Cheltenham Town', 'Chesterfield', "Colchester United", "Crewe Alexandra", "Doncaster Rovers", 'Fleetwood Town', "Gillingham", "Grimsby Town", "Harrogate Town", "Milton Keynes Dons", "Morecambe", "Newport County", "Notts County", 'Port Vale', "Salford City", "Swindon Town", "Tranmere Rovers", "Walsall"]
     df['League'] = None
 
     df.loc[(df['Team within selected timeframe'].isin(step1_2223)) & (df['Season'] == '22/23'), 'League'] = 'Step 1'
     df.loc[(df['Team within selected timeframe'].isin(step1_2324)) & (df['Season'] == '23/24'), 'League'] = 'Step 1'
+    df.loc[(df['Team within selected timeframe'].isin(step1_2425)) & (df['Season'] == '24/25'), 'League'] = 'Step 1'
     df.loc[(df['Team within selected timeframe'].isin(step2_2223)) & (df['Season'] == '22/23'), 'League'] = 'Step 2'
     df.loc[(df['Team within selected timeframe'].isin(step2_2324)) & (df['Season'] == '23/24'), 'League'] = 'Step 2'
+    df.loc[(df['Team within selected timeframe'].isin(step2_2425)) & (df['Season'] == '24/25'), 'League'] = 'Step 2'
     df.loc[(df['Team within selected timeframe'].isin(efl_2223)) & (df['Season'] == '22/23'), 'League'] = 'League Two'
     df.loc[(df['Team within selected timeframe'].isin(efl_2324)) & (df['Season'] == '23/24'), 'League'] = 'League Two'
+    df.loc[(df['Team within selected timeframe'].isin(efl_2425)) & (df['Season'] == '24/25'), 'League'] = 'League Two'
     df.loc[df['Team within selected timeframe'].str.contains('U23|U21|U19|U18'), 'League'] = 'Youth'
     df.loc[df['League'].isnull(), 'League'] = 'Other'
 
@@ -145,3 +161,4 @@ assign_league()
 print(df)
 
 df.to_csv('/Users/as/Library/CloudStorage/GoogleDrive-exyhd1@gmail.com/My Drive/Dorking Wanderers FC Scouting/Recruitment/Pipeline/maindata.csv', index=False)
+df.to_csv('/Users/as/Personal projects/FootballProjects/maindata.csv', index=False)
